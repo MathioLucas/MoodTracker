b611fee0117d8fe45cd66276e62eec534bcdb591
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2025-01-07 19:15:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const moodColors = {
+  happy: '#FFD700',
+  excited:'#FF69B4',
+  neutral:'#A9A9A9',
+  sad:'#4682B4',
+  angry:'#FF6347'
+  }
+
+
+
+
+
+
+
 function getMoodData = () =>{
   const data = localstorage.getitem('moodData');
   return data ? JSON.parse(data) : {};
